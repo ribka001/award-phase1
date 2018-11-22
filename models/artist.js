730e6f6795c2bf2187bf1c03f8dc15c76ec8d66f
Ribka Haryuni <ribka.blue@gmail.com>
2018-11-22 14:22:25 +0700

    creating models Artist, Catergory, ArtistCategory

---
+++ models/artist.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Artist = sequelize.define('Artist', {
+    name: DataTypes.STRING
+  }, {});
+  Artist.associate = function(models) {
+    // associations can be defined here
+  };
+  return Artist;
+};
