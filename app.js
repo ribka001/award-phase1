32d5cfb96950a2d7e3a14444a8e3fe964876f34a
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-11-22 15:13:19 +0700

    add routes and views

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express() 
+const port = 3000
+const router = require('./routes/index')
+
+app.use(express.urlencoded({extended:false}))
+app.use('/',router)
+app.listen(port, function() {
+    console.log("running on port: "+port)
+})
